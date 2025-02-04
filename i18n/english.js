fbc4d66e73a92769cd6f5f2b14d62fa715b09093
TaylorHo <taylor@hotay.dev>
2025-02-04 18:10:55 -0300

    i18n: all content translated

--- i18n/english.js
+++ i18n/english.js
@@ -1,51 +1,122 @@
 export default {
-  "base": "/en",
-  "lang": "en-us",
-  "title": "Arcana Pixel | Online RPG Platform",
-  "description": "A complete virtual RPG table to play online! Created for Paranormal Order campaigns, D&D, and other TTRPGs. Your RPG in a single VTT platform.",
-  "keywords": "RPG, Virtual TableTop, VTT, TTRPG, Play RPG Online, Virtual RPG Table, Paranormal Order, Paranormal Order RPG, D&D, Dungeons and Dragons",
-  "noscript": "To see all the content on this page, you need to enable JavaScript.",
-  "navbar": {
-    "about": "About",
-    "tools": "Tools",
-    "prices": "Prices",
-    "universes": "Universes",
-    "github": "Project Code on GitHub",
-    "proudly_opensource": "We are proudly <a href=\"https://github.com/hotaydev/arcanapixel.com\" target=\"_blank\" rel=\"noopener noreferrer\">Open Source</a>!"
+  base: "/en",
+  lang: "en-us",
+  title: "Arcana Pixel | Online RPG Platform",
+  description: "A complete virtual RPG table to play online! Created for Paranormal Order campaigns, D&D, and other TTRPGs. Your RPG in a single VTT platform.",
+  keywords: "RPG, Virtual TableTop, VTT, TTRPG, Play RPG Online, Virtual RPG Table, Paranormal Order, Paranormal Order RPG, D&D, Dungeons and Dragons",
+  noscript: "To see all the content on this page, you need to enable JavaScript.",
+  navbar: {
+    about: "About",
+    tools: "Tools",
+    prices: "Prices",
+    universes: "Universes",
+    github: "Project Code on GitHub",
+    proudly_opensource: "We are proudly <a href=\"https://github.com/hotaydev/arcanapixel.com\" target=\"_blank\" rel=\"noopener noreferrer\">Open Source</a>!"
   },
-  "main": {
-    "title": "All your RPG in a<br />single platform",
-    "rpg_dice": "RPG Dice",
-    "play_button": "Play!"
+  main: {
+    title: "All your RPG in a<br />single platform",
+    rpg_dice: "RPG Dice",
+    play_button: "Play!"
   },
-  "prices": {
-    "title": "Pricing and Plans for the <abbr title=\"Virtual Table Top\">VTT</abbr> Arcana Pixel",
-    "description": "We will always have a free plan, but if you want to play multiple campaigns, we have more advanced options that might be useful for you!",
-    "eggs": "In the game maps, you may find mysterious items that offer bonuses, such as months of advanced plans or other benefits.",
-    "selfhost": "We also have a \"Hacker\" plan for those who want to host their own version of the platform. By the way, we are Open Source."
+  prices: {
+    title: "Pricing and Plans for the <abbr title=\"Virtual Table Top\">VTT</abbr> Arcana Pixel",
+    description: "We will always have a free plan, but if you want to play multiple campaigns, we have more advanced options that might be useful for you!",
+    eggs: "In the game maps, you may find mysterious items that offer bonuses, such as months of advanced plans or other benefits.",
+    selfhost: "We also have a \"Hacker\" plan for those who want to host their own version of the platform. By the way, we are Open Source."
   },
-  "footer": {
-    "visual_effect": "Star visual effect",
-    "subtitle": "The ultimate platform to run your RPG.<br />Proudly Open Source!",
-    "rights_reserved": "All rights reserved.",
-    "cat_walking": "Cat walking at the bottom of the page in Pixel Art"
+  footer: {
+    visual_effect: "Star visual effect",
+    subtitle: "The ultimate platform to run your RPG.<br />Proudly Open Source!",
+    rights_reserved: "All rights reserved.",
+    cat_walking: "Cat walking at the bottom of the page in Pixel Art"
   },
-  "cta": {
-    "divider": "Divider",
-    "visual_effect": "Star visual effect",
-    "title": "We are still in development!",
-    "your_best_email": "Your best email...",
-    "button": "Get Updates!",
-    "nospam": "Don’t worry, we don’t like spam either.",
-    "pre_register": "Pre-register!!",
-    "content": "Just wait a little longer, and you'll be able to use all the features we're preparing for your campaign (we will have a free version)!<br/><br/>In the meantime, <strong>how about staying up to date with our development updates?</strong><br/><br/>We promise a special gift for those who follow the project from the pre-registration phase!"
+  cta: {
+    divider: "Divider",
+    visual_effect: "Star visual effect",
+    title: "We are still in development!",
+    your_best_email: "Your best email...",
+    button: "Get Updates!",
+    nospam: "Don’t worry, we don’t like spam either.",
+    pre_register: "Pre-register!!",
+    content: "Just wait a little longer, and you'll be able to use all the features we're preparing for your campaign (we will have a free version)!<br/><br/>In the meantime, <strong>how about staying up to date with our development updates?</strong><br/><br/>We promise a special gift for those who follow the project from the pre-registration phase!"
   },
-  "tools": {
-    "title": "Tools designed to make things easier",
-    "map_effect": "Treasure map in Pixel Art",
-    "player_effect": "RPG character in Pixel Art",
-    "player": "Player",
-    "master": "Game Master",
-    "realtime": "Everything in real-time with your friends!"
+  tools: {
+    title: "Tools designed to make things easier",
+    map_effect: "Treasure map in Pixel Art",
+    player_effect: "RPG character in Pixel Art",
+    player: "Player",
+    master: "Game Master",
+    realtime: "Everything in real-time with your friends!"
+  },
+  price_plans: {
+    plan1_title: "Recruit Agent",
+    plan1_item1: "All VTT tools",
+    plan1_item2: "Real-time sessions",
+    plan1_item3: "Up to 6 players per session",
+    plan1_item4: "Up to 4 characters",
+    plan1_item5: "Up to 1 active campaign",
+    plan1_price: "Free",
+
+    plan2_title: "Special Agent",
+    plan2_item1: "All VTT tools",
+    plan2_item2: "Real-time sessions",
+    plan2_item3: "Up to 6 players per session",
+    plan2_item4: "Up to 25 characters",
+    plan2_item5: "Up to 3 active campaigns",
+    plan2_item6: "Exclusive maps",
+    plan2_price: "$5/month",
+
+    plan3_title: "Agent Squad",
+    plan3_item1: "Plan for 6 'Special Agents'",
+    plan3_item2: "Cost split among agents",
+    plan3_item3: "Check out the 'Special Agent' plan",
+    plan3_item4: "$3 per player",
+    plan3_price: "$18/month",
+
+    plan4_title: "Elite Agent",
+    plan4_item1: "All VTT tools",
+    plan4_item2: "Real-time sessions",
+    plan4_item3: "Allows up to 10 players per session",
+    plan4_item4: "Up to 100 characters",
+    plan4_item5: "Up to 10 active campaigns",
+    plan4_item6: "Exclusive maps",
+    plan4_price: "$25/month",
+  },
+  tools_description: {
+    player_tool_1_title: "Create Your Look",
+    player_tool_1_description: "Visually design your character by choosing appearance, clothing, and items. Watch animations for rituals, spells, equipment, attacks, and more!<br /><br />Feel free to equip or unequip items during gameplay.",
+
+    player_tool_2_title: "Exclusive Character Sheet",
+    player_tool_2_description: "Get a character sheet tailored to the RPG you're playing. Rituals, mechanics, and equipment are ready to use.<br /><br />No more memorizing rules.",
+
+    player_tool_3_title: "Inventory and Rituals",
+    player_tool_3_description: "Everything you need to know about your items, spells, and rituals. The platform handles hit rolls, damage, animations, and sound effects.<br /><br />A truly immersive experience without losing the essence of RPG.",
+
+    player_tool_4_title: "Interactive Maps",
+    player_tool_4_description: "Walk through the map, get alerts for investigation checks, see damage effects, and interact with your friends.<br /><br />Everything a map needs.",
+
+    master_tool_1_title: "Create Documents",
+    master_tool_1_description: "Need a document for your investigation? Write the text, choose a template from our gallery, and you're done!<br /><br />Your characters will have a professionally styled document in minutes!",
+
+    master_tool_2_title: "Modular Rules",
+    master_tool_2_description: "All official rules categorized by RPG universe, but that doesn’t mean your campaign has to use them all.<br /><br />Choose which official or optional rules to enable and which to skip!",
+
+    master_tool_3_title: "NPC Generator",
+    master_tool_3_description: "Create unique NPCs with detailed information, appearance, attacks, rituals, and more! Generate allies, enemies, or creatures.<br /><br />Your characters will have an NPC ready in minutes (or seconds)!",
+
+    master_tool_4_title: "Complete Wiki",
+    master_tool_5_description: "No need to memorize equipment, spells, rituals, creatures, or rules.<br /><br />Access any of this information with short descriptions for quick moments and longer descriptions for deep dives.",
+  },
+  universes: {
+    title: "Choose Your Campaign Universe",
+    zombie_credits: "Credits: Enigma of Fear (Game)",
+    cthulhu_credits: "Credits: @chierit on Itch.io",
+    op_description: "Uncover dark mysteries and face creatures from the Other Side. Join Ordo Realitas to protect humanity with rituals, tactics, and courage. The unknown awaits you and your allies.<br><br>This RPG is the foundation of Arcana Pixel, being the first available universe. More worlds will be added soon!",
+    dnd_description: "Venture into magical realms filled with dragons, dungeons, and epic challenges. Create legends with your friends in stories that only your imagination can shape.",
+    cthulhu_description: "Investigate dark secrets that can drive you insane and challenge your sanity. Face cults and cosmic horrors while trying to survive the unknown.",
+    tormenta_description: "Defend Arton from epic dangers and battle the forces of the Tormenta. Be a hero or villain in a world full of battles, intrigue, and unforgettable adventures.",
+    zombie_alt: "Blood Zombie",
+    beholder_alt: "Beholder, a creature from Dungeons & Dragons",
+    tormenta_creature_alt: "Creature from Tormenta RPG"
   }
 }
