d6e220341e44de8ae118d3bac6010de98f9c6da1
TaylorHo <taylor@hotay.dev>
2025-02-14 19:23:17 -0300

    chore: sitemap.xml lastmod change

--- scripts/builder.js
+++ scripts/builder.js
@@ -1,5 +1,5 @@
 import fs from "node:fs/promises";
-import { extname, dirname } from "node:path";
+import { extname, dirname, basename } from "node:path";
 import { minify } from "html-minifier";
 import { glob } from "glob";
 import * as UglifyJS from "uglify-js";
@@ -13,6 +13,14 @@ await fs.rm("./" + distFolder, { recursive: true, force: true });
 await fs.mkdir("./" + distFolder);
 
 const files = await glob(devFolder + '/**/*');
+
+const getCurrentDateForXML = () => {
+  const now = new Date();
+  const offset = '+03:00';
+  const pad = (num) => String(num).padStart(2, '0');
+
+  return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}T${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}${offset}`;
+};
 
 for (const file of files) {
   await handleChangeOnPath(file);
@@ -74,6 +82,13 @@ export default async function handleChangeOnPath(path) {
     const newPathName = path.replace(devFolder, distFolder);
     await fs.mkdir(dirname(newPathName), { recursive: true });
     await fs.writeFile(newPathName, minified.code);
+  } else if (basename(path) === "sitemap.xml") {
+    let fileContent = await fs.readFile(path, "utf-8");
+    const currentDate = getCurrentDateForXML();
+
+    const newPathName = path.replace(devFolder, distFolder);
+    await fs.mkdir(dirname(newPathName), { recursive: true });
+    await fs.writeFile(newPathName, fileContent.replace(/<lastmod>.*?<\/lastmod>/g, `<lastmod>${currentDate}</lastmod>`));
   } else {
     try {
       await fs.stat(path); // Will throw error if the file doesn't exist
